428af8a2720902b9022ae292c9ed1310a463cd31
chandes920 <chandes209@gmail.com>
2022-06-20 16:55:21 -0400

    Update setup.sql

--- postgres/setup.sql
+++ postgres/setup.sql
@@ -3,4 +3,6 @@ CREATE TABLE public.users (
 	fullname VARCHAR ( 100 ) NOT NULL,
 	username VARCHAR ( 50 ) NOT NULL,
 	password VARCHAR ( 255 ) NOT NULL
-)
+);
+
+INSERT INTO public.users values (1, 'Desmond Chan', 'username', 'password');
