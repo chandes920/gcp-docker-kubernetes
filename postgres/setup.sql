404b191641adc4c6150771714c323fc18e161820
chandes920 <chandes209@gmail.com>
2022-06-24 14:06:39 -0400

    Update setup.sql

--- postgres/setup.sql
+++ postgres/setup.sql
@@ -1,8 +1,7 @@
 CREATE TABLE public.users (
 	id serial PRIMARY KEY,
-	fullname VARCHAR ( 100 ) NOT NULL,
 	username VARCHAR ( 50 ) NOT NULL,
 	password VARCHAR ( 255 ) NOT NULL
 );
 
-INSERT INTO public.users values (1, 'Desmond Chan', 'username', 'password');
+INSERT INTO public.users values (1, 'username', 'password');
